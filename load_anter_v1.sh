eb55d7d3fdb877fee79e111f3cbdede8cfabb247
Nanfack Steve <nanfacksteve7@gmail.com>
2022-09-02 10:56:18 +0100

    Remote cleaning and load - OK

--- load_anter_v1.sh
+++ load_anter_v1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# [ Date ]        : 01-09-2022
+# [ Date ]        : 02-09-2022
 # [ Description ] : Ce script sert à AUTOMATISER l'envoie des fichiers PDF avec leurs Méta-données dans Alfresco
 # [Author(s) ]    : NANFACK STEVE
 
 workDir=$1
 
-if [ ! $(echo "${workDir: -1}") = '/' ]; then
+if [ ! "${workDir: -1}" = '/' ]; then
     echo -e "\nErreur: Ajouter un '/' a la fin du dossier\n"
     exit 1
 fi
-if [ ! -d $workDir ]; then
+if [ ! -d "$workDir" ]; then
     echo -e "\nErreur: Dossier inexistant !\n"
     exit 1
 fi
@@ -21,7 +21,7 @@ typeDossierActes=$(echo "$workDir" | awk -F/ '{print $(NF-1)}')
 typeDossierConsolid=$(echo "$workDir" | grep -oE "reprise|digit")
 date=$(date -I)
 #echo "typeConsolid = $typeDossierConsolid | $codeCEC = $codeCEC | typeDossierAct = $typeDossierActes "
-dossierCible=$(echo "/opt/consolidation/$typeDossierConsolid/$codeCEC/$typeDossierActes/$date")
+dossierCible="/opt/consolidation/$typeDossierConsolid/$codeCEC/$typeDossierActes/$date"
 #echo "$dossierCible"
 
 if [ ! -d "$dossierCible" ]; then
@@ -30,16 +30,17 @@ fi
 
 echo -ne "\nNettoyage des fichiers..."
 . cleanFiles_anter_v1.sh 2>>"$workDir"cleanFiles_anter_v1.log
-echo -ne "[ OK ]\n"
+echo -ne " - [ OK ]\n"
 
 fichTraites=0
-echo -ne "\nChargement des donnees dans ALFRESCO..."
+echo -ne "\n"
 for files in $(ls -t | grep "pdf"); do
 
     metaDonnee=$(echo "$files" | sed s/"pdf"/"txt"/g)
-    metaDonnees=$(echo "$workDir$metaDonnee")
-    fileName=$(echo "$workDir$files")
+    metaDonnees="$workDir$metaDonnee"
+    fileName=$"$workDir$files"
     php script_anter_v1.php "$fileName" "$metaDonnees" "$workDir" "$dossierCible" 2>>"$workDir"script_anter_v1.log
-    #echo -ne "($fichTraites)"
+    fichTraites=$((fichTraites + 1))
+    echo -ne "\rChargement des donnees dans ALFRESCO...($fichTraites) envoyée(s)"
 done
-echo -ne "[ OK ]\n\n"
+echo -ne " - [ OK ]\n\n"
