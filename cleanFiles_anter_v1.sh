c5d6903926c4637b8b2d9bf1eca3cfe2805b647a
Nanfack Steve <nanfacksteve7@gmail.com>
2022-09-05 13:14:37 +0100

    suppression des erreur mv

--- cleanFiles_anter_v1.sh
+++ cleanFiles_anter_v1.sh
@@ -4,15 +4,24 @@
 # [ Description ] : Ce script sert à bien renommer les fichiers *.pdf et *.txt
 # [ Author(s) ]   : Mr PROSPERE OTTOU / Mr GAEL MANI / NANFACK STEVE
 
-workDir=$1
+#workDir=$1
 # On remplace les espaces par des "_"
-for file in "$workDir"*.txt; do mv "$file" "${file// /_}"; done
-for file in "$workDir"*.pdf; do mv "$file" "${file// /_}"; done
+for file in *.txt; do
+    if [ "$file" != "${file// /_}" ]; then
+        mv "$file" "${file// /_}"
+    fi
+done
+
+for file in *.pdf; do
+    if [ "$file" != "${file// /_}" ]; then
+        mv "$file" "${file// /_}"
+    fi
+done
 
 # On renomme tous les fichiers qui n'ont pas de num de registre
 i=1
 rplNbr=0
-for file in $(ls -t "$workDir" | grep -E "^[_]**.pdf|^[_]**.txt"); do
+for file in $(ls -t | grep -E "^[_]**.pdf|^[_]**.txt"); do
     if [[ $file == *"pdf"* ]]; then
         #echo "$file" is "pdf"
         mv "$file" "no_num_act_"$i".pdf"
@@ -30,36 +39,46 @@ for file in $(ls -t "$workDir" | grep -E "^[_]**.pdf|^[_]**.txt"); do
 done
 
 # On retire les "_" au debut et la fin des noms de fichiers
-for file in $(ls -t "$workDir" | grep -E "*(_{1,}.txt)$|*(_{1,}.pdf)$"); do
+for file in $(ls -t | grep -E "*(_{1,}.txt)$|*(_{1,}.pdf)$"); do
     newBegin=$(echo $file | sed -E s/"^(_{1,})"//g)
-    mv "$file" "$newBegin"
+    if [ "$file" != "$newBegin" ]; then
+        mv "$file" "$newBegin"
+    fi
 done
 
-for file in $(ls -t "$workDir" | grep -E "*(_{1,}.txt)$"); do
+for file in $(ls -t | grep -E "*(_{1,}.txt)$"); do
     newEnd=$(echo $file | sed -E s/"(_{1,}.txt)$"/.txt/g)
-    mv "$file" "$newEnd"
+    if [ "$file" != "$newBegin" ]; then
+        mv "$file" "$newEnd"
+    fi
 done
 
-for file in $(ls -t "$workDir" | grep -E "*(_{1,}.pdf)$"); do
-    newEnd=$(echo $file | sed -E s/"(_{1,}.pdf)$"/.pdf/g)
-    mv "$file" "$newEnd"
+for file in $(ls -t | grep -E "*(_{1,}.pdf)$"); do
+    newEnd=$(echo "$file" | sed -E s/"(_{1,}.pdf)$"/.pdf/g)
+    if [ "$file" != "$newBegin" ]; then
+        mv "$file" "$newEnd"
+    fi
 done
 
 # On remplace plusieurs "_" par un seul
 
-for file in "$workDir"*.txt; do
+for file in *.txt; do
     newName=$(echo "$file" | sed -E s/"(_{2,})"/_/g)
-    mv "$file" "$newName"
+    if [ "$file" != "$newName" ]; then
+        mv "$file" "$newName"
+    fi
 done
 
-for file in "$workDir"*.pdf; do
+for file in *.pdf; do
     newName=$(echo "$file" | sed -E s/"(_{2,})"/_/g)
-    mv "$file" "$newName"
+    if [ "$file" != "$newName" ]; then
+        mv "$file" "$newName"
+    fi
 done
 
 # Ajout du "#" a la fin du fichier *.txt
-for file in "$workDir"*.txt; do
-    sed s/$/'#'/g "$file" >"$workDir"tmp
-    cat "$workDir"tmp >"$file"
+for file in *.txt; do
+    sed s/$/'#'/g "$file" >tmp
+    cat tmp >"$file"
 done
-rm "$workDir"tmp
+rm tmp
